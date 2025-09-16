af5e4ea7c42341c5a7d00bc40e6a4cc15a34a25b
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-09-16 15:24:35 +0000

    [LeetCode Sync] Runtime - 1 ms (99.69%), Memory - 46 MB (100.00%)

--- my-folder/0004-median-of-two-sorted-arrays/solution.java
+++ my-folder/0004-median-of-two-sorted-arrays/solution.java
@@ -1,42 +1,50 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double res=0;
-
-        int [] arr = new int [nums1.length+nums2.length];
-        int n = arr.length;
-    
-
-        for(int i=0;i<nums1.length;i+=1)
+        if(nums1.length > nums2.length)
         {
-            arr[i]=nums1[i];
+            return findMedianSortedArrays(nums2,nums1);
         }
-
-        for(int i=0;i<nums2.length;i+=1)
-        {
-            arr[i+nums1.length]=nums2[i];
-        }
-
-        Arrays.sort(arr);
-
-        for(int i=0;i<n;i+=1)
-        {
-            System.out.println(arr[i]);
-        }
-
-        int m;
-        m = n/2;
+        int low = 0;
+        int high = nums1.length;
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+        int total = (n1+n2+1)/2;
         
-        if(n%2==0)
+
+        while(low <= high)
         {
-            double a = arr[m-1];
-            double b = arr[m];
-            res = (a+b)/2;
+            int mid = (low+high)/2;
+            int l1 = (mid == 0) ? Integer.MIN_VALUE : nums1[mid-1];
+            int r1 =  (mid == n1)? Integer.MAX_VALUE : nums1[mid];
+            int l2 = (total-mid == 0) ? Integer.MIN_VALUE : nums2[total-mid-1];
+            int r2 = (total-mid == n2) ? Integer.MAX_VALUE : nums2[total-mid];
+
+            if(l1 <= r2 && l2 <= r1)
+            {
+                double ans = 0;
+                if((n1+n2)%2 == 0)
+                {
+                    ans = Math.max(l1,l2) + Math.min(r1,r2);
+                    ans = ans/2;
+                }
+                else
+                {
+                    ans = Math.max(l1,l2);
+                }
+                
+                return ans;
+            }
+            else if(l1 > r2 )
+            {
+                high = mid+-1;                
+            }
+            else
+            {
+                low = mid+1;
+            }
+
         }
-        else
-        {
-            res = arr[m];
-        }
-        
-        return res;
+
+        return 0;
     }
 }
