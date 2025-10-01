17413acac8fa86cb93ad5283bed6e961ff32221a
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-10-01 06:38:11 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 42.7 MB (99.98%)

--- my-folder/0078-subsets/solution.java
+++ my-folder/0078-subsets/solution.java
@@ -1,22 +1,47 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
+    //     <-- Recurrsive pattern -->  
+    //     List<List<Integer>> res = new ArrayList<>();
+    //     List<Integer> part = new ArrayList<>();
+    //     helper(nums,0,res,part);
+    //     return res;
+    // }
+
+    // public void helper(int[] nums,int start,List<List<Integer>> res,List<Integer> part) {
+    //      res.add(new ArrayList<>(part));
+
+    //     for(int i=start;i<nums.length;i+=1)
+    //     {
+    //         part.add(nums[i]);
+    //         helper(nums,i+1,res,part);
+    //         part.remove(part.lastIndexOf(nums[i]));
+    //     }
+
+    //     return;
+
+    // }
+
+
+        // Bitwise operator
+
         List<List<Integer>> res = new ArrayList<>();
-        List<Integer> part = new ArrayList<>();
-        helper(nums,0,res,part);
-        return res;
-    }
+        int n = nums.length;
+        n = (1 << n);
 
-    public void helper(int[] nums,int start,List<List<Integer>> res,List<Integer> part) {
-         res.add(new ArrayList<>(part));
-
-        for(int i=start;i<nums.length;i+=1)
+        for(int i=0;i<n;i+=1)
         {
-            part.add(nums[i]);
-            helper(nums,i+1,res,part);
-            part.remove(part.size()-1);
+            List<Integer> part = new ArrayList<>();
+            int count = 0;
+            while(i >= (1 << count)){
+                if((i & (1 << count)) != 0)
+                {
+                    part.add(nums[count]);
+                }
+                count +=1;
+            }
+            res.add(part);
         }
 
-        return;
-
+        return res;
     }
 }
