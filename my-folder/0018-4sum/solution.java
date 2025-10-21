e9ef9a259cbbfa333531516027a0859ca002da54
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-10-21 14:20:31 +0000

    [LeetCode Sync] Runtime - 19 ms (80.47%), Memory - 43.9 MB (99.98%)

--- my-folder/0018-4sum/solution.java
+++ my-folder/0018-4sum/solution.java
@@ -1,69 +1,36 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
-        
+        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-        if(nums.length < 4 || target == Integer.MAX_VALUE ||target == Integer.MIN_VALUE)
-        {
-            return result;
-        }
- 
-        
-        int low;
-        int high;
 
-        for(int j = 0; j < nums.length - 3 ; j+=1)
-        {
-            if(j>0 && nums[j] == nums[j-1])
-            {
-                continue;
-            }
+        for(int i=0;i<nums.length-3;i+=1){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
 
-            for(int i = j+1;i < nums.length-2;i+=1)
-            {
-                if(i>j+1 && nums[i] == nums[i-1])
-                {
-                  continue;
+            for(int j=i+1;j<nums.length-2;j+=1){
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                int k = j+1;
+                int l = nums.length-1;
+                while(k < l){
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+                    if(sum == target){
+                        res.add(Arrays.asList(nums[i],nums[j],nums[k],nums[l]));
+                        k+=1;
+                        l-=1;
+                        while(k < l && nums[k] == nums[k-1]) k+=1;
+                        while(k < l && nums[l] == nums[l+1]) l-=1;
+                    }
+                    else if(sum < target){
+                        k+=1;
+                        
+                    }
+                    else {
+                        l-=1;
+                        
+                    }
                 }
-
-                low = i+1;
-                high = nums.length-1;
-
-                while(low < high)
-                {
-                    long current_sum = (long)nums[j] + nums[i] +nums[low] + nums[high];
-                    if(current_sum == target)
-                    {
-                        result.add(Arrays.asList(nums[j] , nums[i] , nums[low] , nums[high]));
-                        while(low < high && nums[low] == nums[low+1])
-                        {
-                            low+=1;
-                        }
-                        while(low < high && nums[high] == nums[high-1])
-                        {
-                            high-=1;
-
-                        }
-                        low+=1;
-                        high-=1;
-                    }
-                    else if(current_sum < target)
-                    {
-                        low+=1;
-                    }
-                    else
-                    {
-                        high -=1;
-                    }
-                    
-                }
-
-
             }
         }
 
-        
-        return result;
-        
+        return res;
     }
 }
