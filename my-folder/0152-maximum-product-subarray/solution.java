9f20a4bcdd2acdf1d23e90677903cc57af03d626
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-10-23 17:30:47 +0000

    [LeetCode Sync] Runtime - 1 ms (90.73%), Memory - 45.6 MB (99.92%)

---
+++ my-folder/0152-maximum-product-subarray/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        int prefix = 1;
+        int suffix = 1;
+        int max = Integer.MIN_VALUE;
+        int n = nums.length;
+        for(int i=0;i<n;i+=1){
+            if(prefix == 0) prefix = 1;
+            if(suffix == 0) suffix = 1;
+
+            prefix = prefix * nums[i];
+            suffix = suffix * nums[n-1-i];
+
+            max = Math.max(max,Math.max(suffix,prefix));
+        }
+
+        return max;
+    }
+}
