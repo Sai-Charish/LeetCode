78e377f2ac6087c7b8c8cd0ab7afa75831d450c1
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-11-03 06:24:51 +0000

    [LeetCode Sync] Runtime - 9 ms (68.03%), Memory - 90.4 MB (90.71%)

---
+++ my-folder/1700-minimum-time-to-make-rope-colorful/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minCost(String colors, int[] neededTime) {
+        int totalCost = 0;
+        int prevTime = neededTime[0];
+
+        for (int i = 1; i < colors.length(); i++) {
+            if (colors.charAt(i) == colors.charAt(i - 1)) {
+                totalCost += Math.min(prevTime, neededTime[i]);
+                prevTime = Math.max(prevTime, neededTime[i]);
+            } else {
+                prevTime = neededTime[i];
+            }
+        }
+        return totalCost;
+    }
+}
+
