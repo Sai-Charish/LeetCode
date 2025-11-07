4a2b994238180e6899450ad2adad805d4abbea6d
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-11-07 12:47:14 +0000

    [LeetCode Sync] Runtime - 1454 ms (36.57%), Memory - 0.0B (100.00%)

---
+++ my-folder/0607-sales-person/solution.sql
@@ -0,0 +1,9 @@
+SELECT name
+FROM SalesPerson 
+WHERE name NOT IN (SELECT s.name
+FROM Orders o
+LEFT JOIN Company c
+ON o.com_id = c.com_id
+LEFT JOIN SalesPerson s
+ON o.sales_id = s.sales_id
+WHERE c.name = 'RED');
