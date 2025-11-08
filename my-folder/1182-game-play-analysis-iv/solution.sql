547e61ae3c1ecf6b0b79ff7b68f9a1253879bb4e
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-11-08 13:28:56 +0000

    [LeetCode Sync] Runtime - 640 ms (21.50%), Memory - 0.0B (100.00%)

---
+++ my-folder/1182-game-play-analysis-iv/solution.sql
@@ -0,0 +1,8 @@
+
+SELECT ROUND(COUNT(1)/(SELECT COUNT(DISTINCT player_id) FROM Activity),2) as fraction
+FROM Activity
+WHERE (player_id, DATE_SUB(event_date,INTERVAL 1 DAY)) IN 
+(
+    SELECT player_id , min(event_date) as first_date
+FROM Activity 
+GROUP BY player_id);
