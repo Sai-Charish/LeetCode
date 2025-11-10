8ec2aa975081169f21f2d34c0e0daf968d7006f6
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-11-10 04:29:38 +0000

    [LeetCode Sync] Runtime - 562 ms (50.96%), Memory - 0.0B (100.00%)

---
+++ my-folder/0585-investments-in-2016/solution.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+
+select round(sum(tiv_2016),2) as tiv_2016
+from Insurance
+Where tiv_2015 in (
+    select tiv_2015
+    from Insurance
+    group by tiv_2015
+    having count(1) > 1
+) AND
+      (lat,lon) not in (
+        select lat , lon 
+        from Insurance
+        group by lat , lon
+        having count(1) > 1
+)
+
