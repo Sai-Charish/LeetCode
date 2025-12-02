caf2fee027daf2b8361be972d9d14f533291b767
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-12-02 06:12:54 +0000

    [LeetCode Sync] Runtime - 10 ms (24.59%), Memory - 49 MB (72.10%)

--- my-folder/0056-merge-intervals/solution.java
+++ my-folder/0056-merge-intervals/solution.java
@@ -1,27 +1,27 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals,(a,b)-> a[0] - b[0]);
-        int n= intervals.length;
-        List<int[]> Store = new ArrayList<>();
-        
-        Store.add(intervals[0]);    
-        int size = 1  ;
-        for(int i=1;i<n;i+=1){
-            int [] last = Store.get(size-1);
+        Arrays.sort(intervals,(a,b) -> a[0] - b[0]);
+        ArrayList<int[]> store = new ArrayList<>();
 
+        store.add(intervals[0]);
+        int size = 1;
+
+        for(int i=1;i<intervals.length;i+=1){
+            int [] last = store.get(size-1);
             if(last[1] < intervals[i][0]){
-                Store.add(intervals[i]);
-                size+=1;
+                store.add(intervals[i]);
+                size+=1;;
             }
             else {
                 last[1] = Math.max(last[1],intervals[i][1]);
             }
         }
-        
+
+
         int [][] res = new int [size][2];
 
-        for(int i=0;i<res.length;i+=1){
-            res[i] = Store.get(i);
+        for(int i=0;i<size;i+=1){
+            res[i] = store.get(i);
         }
 
         return res;
