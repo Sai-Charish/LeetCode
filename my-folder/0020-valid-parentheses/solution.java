9fb60ddf6ee92702139fd2b90c007784f0b9d30f
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-12-02 06:31:46 +0000

    [LeetCode Sync] Runtime - 3 ms (87.51%), Memory - 43.5 MB (41.27%)

--- my-folder/0020-valid-parentheses/solution.java
+++ my-folder/0020-valid-parentheses/solution.java
@@ -1,32 +1,35 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> st = new Stack<>();
+
         for(int i=0;i<s.length();i+=1) {
             char ch = s.charAt(i);
-
-            if(st.isEmpty() && (ch == ')' || ch == ']' || ch == '}')) {
-                return  false;
-            }
-
-            if(ch == '(' || ch == '{' || ch == '[') st.push(ch);
-
-            else{
-                char top = st.peek();
-                if(top == '(' && ch == ')') {
-                    st.pop();
-                }
-                else if(top == '{' && ch == '}') {
-                    st.pop();
-                }
-                else if(top == '[' && ch == ']') {
-                    st.pop();
-                }
-                else {
+            if(ch == '}' || ch == ')' || ch == ']'){
+                 if(st.isEmpty()){
                     return false;
                 }
+                else{
+                    char top = st.peek();
+                    if(top == '(' && ch == ')'){
+                        st.pop();
+                    }
+                    else if(top == '{' && ch == '}'){
+                        st.pop();
+                    }
+                    else if(top == '[' && ch == ']'){
+                        st.pop();
+                    }
+                    else {
+                        st.push(ch);
+                    }
+                }
             }
+            else {
+                st.push(ch);
+            }
+            
+           
         }
         return st.isEmpty();
-        
     }
 }
