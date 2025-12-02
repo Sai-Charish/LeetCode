a9a1c26ece65cb2126582ac86ccfddcb51f2e27e
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-12-02 06:55:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 47.3 MB (66.03%)

--- my-folder/0268-missing-number/solution.java
+++ my-folder/0268-missing-number/solution.java
@@ -1,42 +1,17 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        int n = nums.length-1;
+        int actualSum = (n * (n+1))/2;
 
-        
-        int n = nums.length - 1;
-        int max = nums[0];
-
-        boolean Zero_checker = false;
-
-        int Curr_sum = 0;
-
-        for(int i=0;i<=n;i+=1)
-        {
-
-            Curr_sum+=nums[i];
-            max = Math.max(max,nums[i]);
-
-            if(nums[i] == 0)
-            {
-                Zero_checker = true;
-            }
+        int currSum = 0;
+        int max = 0;
+        for(int i : nums){
+            currSum += i;
+            max = Math.max(max,i);
         }
-
-        int Actual_sum = (max*(max+1))/2;
-
-        System.out.println(Actual_sum);
-        System.out.println(Curr_sum);
-
-        if(Actual_sum == Curr_sum && Zero_checker)
-        {
-            return max + 1;
+        if(max == n){
+            return max+1;
         }
-        else if(Actual_sum == Curr_sum)
-        {
-            return 0;
-        }
-
-
-        return Actual_sum - Curr_sum;
-
+        return Math.abs(actualSum - currSum + max);
     }
 }
