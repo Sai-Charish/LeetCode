7399d5878a70c67dd1a2772b5ca20c49d4330ea9
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-12-05 07:47:45 +0000

    [LeetCode Sync] Runtime - 5 ms (39.60%), Memory - 45.5 MB (76.10%)

--- my-folder/0120-triangle/solution.java
+++ my-folder/0120-triangle/solution.java
@@ -1,31 +1,38 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int n = triangle.size();
-        int [][] dp = new int [n+1][n+1];
-        for(int i=n-1;i>=0;i-=1){
-            int m = triangle.get(i).size();
-            for(int j=m-1;j>=0;j-=1){
-                int cost = triangle.get(i).get(j);
-                dp[i][j] = cost + Math.min(dp[i+1][j],dp[i+1][j+1]);
+        int m = triangle.get(n-1).size();
+        int [][] dp = new int [n][m];
+        for(int i=0;i<m;i+=1){
+            dp[n-1][i] = triangle.get(n-1).get(i);
+        }
+
+        // return helper(triangle,n,0,0,dp);
+
+        for(int i=n-2;i>=0;i-=1){
+            for(int j=triangle.get(i).size()-1;j>=0;j-=1){
+                int left = triangle.get(i).get(j) + dp[i+1][j];
+                int right = triangle.get(i).get(j) + dp[i+1][j+1];
+
+                dp[i][j] = Math.min(left,right);
             }
         }
+
         return dp[0][0];
-        // return helper(triangle,n,0,0,dp);
     }
 
-    public int helper(List<List<Integer>> triangle,int n,int i,int j,List<List<Integer>> dp){
-        //base case
-        if(i == n){
+    public int helper(List<List<Integer>> triangle,int n,int i,int j,int [][] dp){
+        if(i == triangle.size() || j == triangle.get(i).size()){
             return 0;
         }
 
-        if(dp.get(i).get(j) != Integer.MAX_VALUE){
-            return dp.get(i).get(j);
+        if(dp[i][j] != Integer.MAX_VALUE){
+            return dp[i][j];
         }
-        int left =  triangle.get(i).get(j) + helper(triangle,n,i+1,j,dp);
+
+        int left = triangle.get(i).get(j) + helper(triangle,n,i+1,j,dp);
         int right = triangle.get(i).get(j) + helper(triangle,n,i+1,j+1,dp);
-        int ans = Math.min(left,right);
-        dp.get(i).set(j,ans);
-        return ans; 
+
+        return dp[i][j] = Math.min(left,right);
     }
 }
