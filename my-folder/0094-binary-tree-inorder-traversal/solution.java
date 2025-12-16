faf9d5e62e632149033acfb3223f507038441f82
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2025-12-16 14:21:03 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 43.2 MB (50.39%)

---
+++ my-folder/0094-binary-tree-inorder-traversal/solution.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        TreeNode curr = root;
+
+        while(curr != null){
+            if(curr.left == null){
+                res.add(curr.val);
+                curr = curr.right;
+            }
+            else{
+                TreeNode ip = predecessor(curr.left,curr);
+                if(ip.right == null){
+                    ip.right = curr;
+                    curr = curr.left;
+                }
+                else{
+                    ip.right = null;
+                    res.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+        }
+        return res;
+    }
+
+    public TreeNode predecessor(TreeNode root,TreeNode curr){
+        while(root.right != null && root.right != curr){
+            root = root.right;
+        }
+        return root;
+    }
+}
