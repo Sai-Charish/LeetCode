e4d9ad9ccd9ee6756f109e2311066bf30ed5597d
Ananthoji Sai Charish <145242929+Sai-Charish@users.noreply.github.com>
2026-02-20 13:15:17 +0000

    [LeetCode Sync] Runtime - 2 ms (99.94%), Memory - 43.5 MB (14.67%)

---
+++ my-folder/0763-special-binary-string/solution.java
@@ -0,0 +1,33 @@
+class Solution {
+    public String makeLargestSpecial(String s) {
+        if (s.length() <= 2)
+            return s;
+
+        int count = 0;
+        int start = 0;
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '1')
+                count++;
+            else
+                count--;
+
+            if (count == 0) {
+                String inner = makeLargestSpecial(s.substring(start + 1, i));
+                list.add("1" + inner + "0");
+                start = i + 1;
+            }
+        }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        StringBuilder result = new StringBuilder();
+        for (String str : list) {
+            result.append(str);
+        }
+
+        return result.toString();
+    }
+}
+
